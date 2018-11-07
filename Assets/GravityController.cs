682fb649fe3d1a61c9142fec8dbd1fa829ddefcc
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-11-08 00:53:45 +0900

    gravity game の実装

---
+++ Assets/GravityController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    [SerializeField] float m_accelarationOfGravity = 9.81f;
+    [SerializeField] Transform m_centerOfGravity;
+    Transform m_player;
+
+    void Start()
+    {
+        m_player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                GameObject go = hit.collider.gameObject;
+                if (go.tag == "GravitySource")
+                {
+                    m_centerOfGravity = go.transform;
+                }
+            }
+        }
+
+        Vector3 dir = m_centerOfGravity.position - m_player.position;
+        Physics.gravity = dir.normalized * m_accelarationOfGravity;
+        m_player.up = -1 * dir;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float m_moveAccelaration = 1.0f;
+    Rigidbody m_rb;
+
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float v = CrossPlatformInputManager.GetAxisRaw("Vertical");
+        float h = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+        if (v != 0)
+        {
+            m_rb.AddForce(v * transform.forward * m_moveAccelaration, ForceMode.Force);
+        }
+        if (h != 0)
+        {
+            m_rb.AddForce(h * transform.right * m_moveAccelaration, ForceMode.Force);
+        }
+    }
+}
